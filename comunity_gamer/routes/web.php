388f59eeea7eb50e1173a568911890710ad321b5
kerlon_velasquez <kerlonvelasquez525@gmail.com>
2026-08-24 16:51:47 -0500

    cambios clase del 24 agost

--- comunity_gamer/routes/web.php
+++ comunity_gamer/routes/web.php
@@ -2,6 +2,11 @@
 
 use App\Http\Middleware\EnsureTeamMembership;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NoticiasController;
+use App\Http\Controllers\ComentariosController;
+use App\Http\Controllers\ComunidadController;
+use App\Http\Controllers\ProblemasController;
 
 Route::view('/', 'welcome')->name('home');
 
@@ -9,6 +14,13 @@ Route::prefix('{current_team}')
     ->middleware(['auth', 'verified', EnsureTeamMembership::class])
     ->group(function () {
         Route::view('dashboard', 'dashboard')->name('dashboard');
+        
+        // Tus rutas de recursos:
+        Route::resource('home', HomeController::class);
+        Route::resource('noticias', NoticiasController::class);
+        Route::resource('comentarios', ComentariosController::class);
+        Route::resource('comunidad', ComunidadController::class);
+        Route::resource('problemas', ProblemasController::class);
     });
 
 require __DIR__.'/settings.php';

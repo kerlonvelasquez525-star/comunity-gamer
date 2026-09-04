328b92e48bc0209a223e23c38d1f41b4191ef6d0
kerlon_velasquez <kerlonvelasquez525@gmail.com>
2026-09-04 07:57:06 -0500

    feat(perfil): agregar perfil profesional de kerlon velasquez/angel quintana

--- js/aprendices_db.js
+++ js/aprendices_db.js
@@ -32,108 +32,88 @@ const APRENDICES_DATA = [
     },
     "fraseFavorita": "La práctica constante y la disciplina convierten el conocimiento en maestría.",
     "estado": "Guiando la sesión de aprendizaje"
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-    
   },
   {
-  "id": "yojhan-felipe-forero-trivino",
-  "nombreCompleto": "yojhan felipe forero triviño",
-  "ficha": "3294152",
-  "programa": "Análisis y Desarrollo de Software",
-  "rol": "Aprendiz / Desarrollador Junior",
-  "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación. futuro programador senior",
-=======
-=======
-    
->>>>>>> 663d27942db0658838590c42452b5ee43523b9e8
+    "id": "yojhan-felipe-forero-trivino",
+    "nombreCompleto": "yojhan felipe forero triviño",
+    "ficha": "3294152",
+    "programa": "Análisis y Desarrollo de Software",
+    "rol": "Aprendiz / Desarrollador Junior",
+    "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación. futuro programador senior",
+    "habilidades": [
+      "Git",
+      "GitHub",
+      "HTML5",
+      "CSS3"
+    ],
+    "intereses": [
+      "Desarrollo Web",
+      "Programación"
+    ],
+    "avatar": "assets/avatares/avatar_default.svg",
+    "redes": {
+      "github": "https://github.com/yojhandev",
+      "linkedin": "https://linkedin.com/in/yojhandev",
+      "portafolio": "https://yojhandev.github.io/mi-perfil-sena"
+    },
+    "fraseFavorita": "Sin mente que pensar.  Sin voluntad que quebrar.  Sin voz que clame por el sufrimiento.  Nacido de Dios y del Vacío.  Tú sellarás la luz cegadora que devora sus sueños.  Tú eres el Recipiente.  Tú eres el Hollow Knight.",
+    "estado": "Listo para colaborar"
   },
-
   {
-  "id": "andres-martinez",
-  "nombreCompleto": "Andres Martinez",
-  "ficha": "3294152",
-  "programa": "Análisis y Desarrollo de Software",
-  "rol": "Aprendiz / Desarrollador Junior",
-  "biografia": "Aprendiz SENA interesado en aprender cosas nuevas y trabajar en equipo",
-=======
+    "id": "andres-martinez",
+    "nombreCompleto": "Andres Martinez",
+    "ficha": "3294152",
+    "programa": "Análisis y Desarrollo de Software",
+    "rol": "Aprendiz / Desarrollador Junior",
+    "biografia": "Aprendiz SENA interesado en aprender cosas nuevas y trabajar en equipo",
+    "habilidades": [
+      "Git",
+      "GitHub",
+      "HTML5",
+      "CSS3"
+    ],
+    "intereses": [
+      "Desarrollo Web",
+      "Programación"
+    ],
+    "avatar": "assets/avatares/avatar_carlos.svg",
+    "redes": {
+      "github": "https://github.com/andresdevapps",
+      "linkedin": "https://linkedin.com/in/andresdevapps",
+      "portafolio": "https://andresdevapps.github.io/mi-perfil-sena"
+    },
+    "fraseFavorita": "Mejor intentar y fallar que no intentar.",
+    "estado": "Listo para colaborar"
   },
-{
-  "id": "cristian-david-lemus-ovalle",
-  "nombreCompleto": "Cristian David Lemus Ovalle",
-  "ficha": "3294152",
-  "programa": "Análisis y Desarrollo de Software",
-  "rol": "Aprendiz / Desarrollador Junior",
-<<<<<<< HEAD
-  "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación.",
-=======
-  "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación en constante aprendizaje.",
->>>>>>> 81883fdcf9cddb0a758df21a91ce751edf75193b
-<<<<<<< HEAD
-=======
->>>>>>> 15ed4fabceff8672970d01bd393817188c5a4d20
->>>>>>> 663d27942db0658838590c42452b5ee43523b9e8
-  "habilidades": [
-    "Git",
-    "GitHub",
-    "HTML5",
-<<<<<<< HEAD
-    "CSS3"
-=======
-<<<<<<< HEAD
-    "CSS3"
-=======
-    "CSS3",
-    "JavaScript"
->>>>>>> 81883fdcf9cddb0a758df21a91ce751edf75193b
->>>>>>> 15ed4fabceff8672970d01bd393817188c5a4d20
-  ],
-  "intereses": [
-    "Desarrollo Web",
-    "Programación"
-  ],
-<<<<<<< HEAD
-  "avatar": "assets/avatares/avatar_default.svg",
-  "redes": {
-    "github": "https://github.com/yojhandev",
-    "linkedin": "https://linkedin.com/in/yojhandev",
-    "portafolio": "https://yojhandev.github.io/mi-perfil-sena"
+  {
+    "id": "cristian-david-lemus-ovalle",
+    "nombreCompleto": "Cristian David Lemus Ovalle",
+    "ficha": "3294152",
+    "programa": "Análisis y Desarrollo de Software",
+    "rol": "Aprendiz / Desarrollador Junior",
+    "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación en constante aprendizaje.",
+    "habilidades": [
+      "Git",
+      "GitHub",
+      "HTML5",
+      "CSS3",
+      "JavaScript"
+    ],
+    "intereses": [
+      "Desarrollo Web",
+      "Programación"
+    ],
+    "avatar": "assets/avatares/avatar_default.svg",
+    "redes": {
+      "github": "https://github.com/kerlonvelasquez525-star",
+      "linkedin": "https://www.linkedin.com/in/kerlon-velasquez-72180b429/",
+      "portafolio": "https://github.com/kerlonvelasquez525-star/crear_componentes_frontend"
+    },
+    "fraseFavorita": "No eres un dios... ¡pero aun así me deleitaré con tu esencia!",
+    "estado": "Listo para colaborar"
   },
-  "fraseFavorita": "Sin mente que pensar.  Sin voluntad que quebrar.  Sin voz que clame por el sufrimiento.  Nacido de Dios y del Vacío.  Tú sellarás la luz cegadora que devora sus sueños.  Tú eres el Recipiente.  Tú eres el Hollow Knight.",
-  "estado": "Listo para colaborar"
-},
-
-  
-=======
-  "avatar": "assets/avatares/avatar_carlos.svg",
-  "redes": {
-<<<<<<< HEAD
-    "github": "https://github.com/Anfixsinax",
-    "linkedin": "https://linkedin.com/in/Anfixsinax",
-    "portafolio": "https://Anfixsinax.github.io/mi-perfil-sena"
-=======
-<<<<<<< HEAD
-    "github": "https://github.com/andresdevapps",
-    "linkedin": "https://linkedin.com/in/andresdevapps",
-    "portafolio": "https://andresdevapps.github.io/mi-perfil-sena"
-  },
-  "fraseFavorita": "Mejor intentar y fallar que no intentar.",
-  "estado": "Listo para colaborar"
-}
-=======
-    "github": "https://github.com/kerlonvelasquez525-star",
-    "linkedin": "https://www.linkedin.com/in/kerlon-velasquez-72180b429/",
-    "portafolio": "https://github.com/kerlonvelasquez525-star/crear_componentes_frontend"
->>>>>>> 15ed4fabceff8672970d01bd393817188c5a4d20
-  },
-  "fraseFavorita": "No eres un dios... ¡pero aun así me deleitaré con tu esencia!",
-  "estado": "Listo para colaborar"
-<<<<<<< HEAD
-}
-=======
-},
-{    "id": "angel-quintana",
+  {
+    "id": "angel-quintana",
     "nombreCompleto": "Angel Quintana",
     "ficha": "3294152",
     "programa": "Análisis y Desarrollo de Software",
@@ -158,15 +138,41 @@ const APRENDICES_DATA = [
     },
     "fraseFavorita": "La práctica constante hace al maestro del código y mateo es un peo",
     "estado": "Listo para colaborar"
-<<<<<<< HEAD
   },
   {
-  "id": "yoiner-esteban-romero-cardenas",
-  "nombreCompleto": "Yoiner Esteban Romero Cardenas",
+    "id": "yoiner-esteban-romero-cardenas",
+    "nombreCompleto": "Yoiner Esteban Romero Cardenas",
+    "ficha": "3294152",
+    "programa": "Análisis y Desarrollo de Software",
+    "rol": "Aprendiz / Desarrollador Junior",
+    "biografia": "Aprendiz SENA, desarrollo web y el aprendizaje de nuevas tecnologías de programación, proximo desarrollador fullstack.",
+    "habilidades": [
+      "Git",
+      "GitHub",
+      "HTML5",
+      "CSS3",
+      "JavaScript"
+    ],
+    "intereses": [
+      "Desarrollo Web",
+      "Programación"
+    ],
+    "avatar": "assets/avatares/avatar_carlos.svg",
+    "redes": {
+      "github": "https://github.com/YoiDevops",
+      "linkedin": "https://linkedin.com/in/YoiDevops",
+      "portafolio": "https://YoiDevops.github.io/mi-perfil-sena"
+    },
+    "fraseFavorita": "Yo solía ser un desarrollador como tú, pero entonces recibí un virus en el ordenador.",
+    "estado": "Listo para colaborar"
+  },
+  {
+  "id": "kerlon-velasquez",
+  "nombreCompleto": "Kerlon Velasquez",
   "ficha": "3294152",
   "programa": "Análisis y Desarrollo de Software",
   "rol": "Aprendiz / Desarrollador Junior",
-  "biografia": "Aprendiz SENA, desarrollo web y el aprendizaje de nuevas tecnologías de programación, proximo desarrollador fullstack.",
+  "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación en constante aprendizaje.",
   "habilidades": [
     "Git",
     "GitHub",
@@ -180,21 +186,14 @@ const APRENDICES_DATA = [
   ],
   "avatar": "assets/avatares/avatar_carlos.svg",
   "redes": {
-    "github": "https://github.com/YoiDevops",
-    "linkedin": "https://linkedin.com/in/YoiDevops",
-    "portafolio": "https://YoiDevops.github.io/mi-perfil-sena"
+    "github": "https://github.com/kerlonvelasquez525-star",
+    "linkedin": "https://www.linkedin.com/in/kerlon-velasquez-72180b429/",
+    "portafolio": "https://github.com/kerlonvelasquez525-star/crear_componentes_frontend"
   },
-  "fraseFavorita": "Yo solía ser un desarrollador como tú, pero entonces recibí un virus en el ordenador.",
+  "fraseFavorita": "La práctica constante hace al maestro del código.",
   "estado": "Listo para colaborar"
 }
-=======
-  }
->>>>>>> 81883fdcf9cddb0a758df21a91ce751edf75193b
-<<<<<<< HEAD
-=======
->>>>>>> 15ed4fabceff8672970d01bd393817188c5a4d20
->>>>>>> 663d27942db0658838590c42452b5ee43523b9e8
- ];
+];
 
 // Si se ejecuta en entorno Node/módulos
 if (typeof module !== 'undefined' && module.exports) {
